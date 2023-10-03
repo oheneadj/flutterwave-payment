d5bfa67567899175aaf9f2b3763e389c5b4721e7
Ohene Adjei <oheneadjei.dev@gmail.com>
2023-10-03 18:40:01 +0000

    added route for getting single transfer

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const needle = require("needle");
 const Flutterwave = require("flutterwave-node-v3");
 
-router.get("/", async (req, res) => {
+router.post("/", async (req, res) => {
   //Check if public key and secret key are available
   if (
     process.env.FLUTTERWAVE_PUBLIC_KEY == "" ||
@@ -20,18 +20,47 @@ router.get("/", async (req, res) => {
       process.env.FLUTTERWAVE_PUBLIC_KEY,
       process.env.FLUTTERWAVE_SECRET_KEY
     );
+    //const details = [...req.body];
     const details = {
-      account_bank: "044",
-      account_number: "0690000040",
-      amount: 200,
-      currency: "NGN",
-      narration: "Payment for things",
+      account_bank: "MTN",
+      account_number: "2250757564300",
+      amount: 2,
+      currency: "GHS",
+      beneficiary_name: "ASSOA Arnauld",
     };
 
     const data = await flw.Transfer.initiate(details);
 
     res.json({ data });
   }
+});
+
+router.get("/:id", async (req, res) => {
+  //check if the url has an id
+  if (req.params.id != "") {
+    if (
+      process.env.FLUTTERWAVE_PUBLIC_KEY == "" ||
+      process.env.FLUTTERWAVE_SECRET_KEY == ""
+    ) {
+      //if not available send response
+      return res.json({
+        message: "PUBLIC KEY and SECRET KEY cannot be blank",
+      });
+    } else {
+      //create instance of Flutterwave
+      const flw = new Flutterwave(
+        process.env.FLUTTERWAVE_PUBLIC_KEY,
+        process.env.FLUTTERWAVE_SECRET_KEY
+      );
+
+      const data = await flw.Transfer.get_a_transfer({ id: req.params.id });
+
+      res.json({ data });
+    }
+  } else {
+    res.json({ message: "You must provide a transaction id" });
+  }
+  //Check if public key and secret key are available
 });
 
 module.exports = router;
