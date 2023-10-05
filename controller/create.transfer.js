bf53c84440175f5a142f345a4ebd52901264340f
Ohene Adjei <oheneadjei.dev@gmail.com>
2023-10-05 00:39:54 +0000

    added cron scheduler

--- controller/create.transfer.js
+++ controller/create.transfer.js
@@ -1,4 +1,5 @@
 const Flutterwave = require("flutterwave-node-v3");
+const cron = require("node-cron");
 
 const createTransfer = async (req, res) => {
   if (
@@ -38,11 +39,53 @@ const createTransfer = async (req, res) => {
 
     try {
       const response = await flw.Transfer.initiate(details);
+
+      //LOG THIS STRING AFTER A NEW TRANSFER IS INITIATED
+      console.log("A NEW TRANSFER HAS BEEN CREATED");
+
+      //LOG THE TRANSFER RESPONSE INTO THE CONSOLE
+      console.log(response);
+
       res.json({ response });
     } catch (error) {
       res.json({ error });
     }
   }
 };
+// This CRON Scheduler calls the createTranfer method every minute
+cron.schedule("* * * * *", createTransfer);
+
+/**
+ *
+ * These asterisks are part of the 
+ * crontab syntax to represent different units of time:
+
+  * * * * * *
+  | | | | | |
+  | | | | | day of week
+  | | | | month
+  | | | day of month
+  | | hour
+  | minute
+  second ( optional )
+
+  VISIT https://crontab.guru 
+  TO LEARN MORE
+
+A single asterisk behaves like a wildcard. 
+Meaning the task will be run for every 
+instance of that unit of time. 
+Five asterisks ('* * * * *') represents the
+ crontab default of running every minute.
+
+Numbers in the place of asterisks will be 
+treated as values for that unit of time. 
+Allowing you to schedule tasks to occur 
+daily and weekly or in more complex. 
+
+
+ * 
+ *
+ */
 
 module.exports = createTransfer;
